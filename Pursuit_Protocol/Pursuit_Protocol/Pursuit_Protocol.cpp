af5fe7b78462ff2f54e2e678ad2486d6b15dec79
Sid <7568554+unrealsid@users.noreply.github.com>
2025-08-11 22:35:51 +0530

    Initial env commit. Added CMake file and other basic project files Also added Placeholder.txt files for resources and shaders. Will remove those later

---
+++ Pursuit_Protocol/Pursuit_Protocol/Pursuit_Protocol.cpp
@@ -0,0 +1,12 @@
+﻿// Pursuit_Protocol.cpp : Defines the entry point for the application.
+//
+
+#include "Pursuit_Protocol.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
